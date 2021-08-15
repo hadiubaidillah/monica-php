d14071a5b6e8a64d2d724c73ae777926071f5969
Hadi Ubaidillah <ubaycreative@gmail.com>
2021-08-15 10:07:55 +0700

    MONICA

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="es">
 	<head>
 		<meta charset="UTF-8">
-		<meta name="description" content="Google search engine"/>
+		<meta name="description" content="Monica search engine"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="robots" content="index,follow"/>
